c4d1dcccc7f654569ce5c1272aaefef0f5e7250e
LGA1366 <54014515+LGA1366@users.noreply.github.com>
2020-03-04 01:30:09 +0200

    1

--- scripts/script.js
+++ scripts/script.js
@@ -23,36 +23,36 @@ $(document).ready(function(){
         $(this).parents('html').toggleClass('show__burger');
 
     });
-    //     const baseUrl = "http://data.fixer.io/api/latest?access_key=",
-    //           key = "fede7e12e787ff5ee406bcaa05ef258f",
-    //           eurRow = $('.js__eur__exchange'),
-    //           usdRow = $('.js__usd__exchange');
+        const baseUrl = "http://data.fixer.io/api/latest?access_key=",
+              key = "fede7e12e787ff5ee406bcaa05ef258f",
+              eurRow = $('.js__eur__exchange'),
+              usdRow = $('.js__usd__exchange');
         
-    //     let eurRate, usdRate, usd;
+        let eurRate, usdRate, usd;
 
-    //     $.ajax({
-    //         url: baseUrl+key,   
-    //         dataType: 'jsonp',
-    //         success: function(json) {
+        $.ajax({
+            url: baseUrl+key,   
+            dataType: 'jsonp',
+            success: function(json) {
                 
-    //             if (!json.success) {
-    //                 eurRow.append(
-    //                 `<td class="unavailable" rowspan="2">Сервис временно недоступен</td>`
-    //                 );
-    //                 usdRow.css("border-bottom","none");
-    //                 eurRow.css("border-bottom","none");
-    //                 console.log(json.error.info);
-    //             } else {
-    //                 eurRate = json.rates.RUB.toFixed(4);
-    //                 usd = json.rates.USD;
-    //                 usdRate = (eurRate/usd).toFixed(4);
-    //                 eurRow.append(
-    //                         `<td>${eurRate}</td>`
-    //                 );
-    //                 usdRow.append(
-    //                         `<td>${usdRate}</td>`
-    //                 );   
-    //             }          
-    //         }
-    //     });
+                if (!json.success) {
+                    eurRow.append(
+                    `<td class="unavailable" rowspan="2">Сервис временно недоступен</td>`
+                    );
+                    usdRow.css("border-bottom","none");
+                    eurRow.css("border-bottom","none");
+                    console.log(json.error.info);
+                } else {
+                    eurRate = json.rates.RUB.toFixed(4);
+                    usd = json.rates.USD;
+                    usdRate = (eurRate/usd).toFixed(4);
+                    eurRow.append(
+                            `<td>${eurRate}</td>`
+                    );
+                    usdRow.append(
+                            `<td>${usdRate}</td>`
+                    );   
+                }          
+            }
+        });
 });
