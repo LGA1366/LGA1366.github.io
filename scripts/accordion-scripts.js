795693cff07a6626091ca89b6ed0727ae8e15815
LGA1366 <54014515+LGA1366@users.noreply.github.com>
2021-01-16 21:32:35 +0200

    1
    1

---
+++ scripts/accordion-scripts.js
@@ -0,0 +1,11 @@
+// accordion
+
+$( function() {
+  $( ".accordion" ).accordion({
+    collapsible: true,
+    active: 6,
+    icons: null,
+    header: "button",
+    heightStyle: "content"
+  });
+});
